75b11c5b5cd377748fc61748614a93642fb70b08
NopsyNight <109688607+NopsyNight@users.noreply.github.com>
2025-10-20 15:19:45 -0300

    Arquivos das aulas 10 e 11
    Não há arquivos das aulas 8 e 9 pois são respectivamentes teoria sobre como o java funciona, e suas aplicações, mas descrevendo cada uma
    
    Aula 08 foi a introdução da instalação da IDE (Integrated Development Environment) ou Ambiente Integrado de Desenvolvimento, onde instalei o aplicativo Eclipse, o curso introduz tanto o Eclipse e Netbeans, e minha escolha foi o Eclipse.
    
    Aula 09 Foi a demonstração de como colocar arquivos e mexer um pouco na IDE, além de introduzir uma coisa que é muito trabalhada no Java que é a introdução à Classe e Objetos, onde a professora descreve como o java dá o valor a uma classe e dentro dessa classe existe vários tipos de informações que são guardados respectivos a essa classe, como um animal e dentro dessa classe animal, encontramos espécies e vai descendo, e os Objetos são como se fosse uma informação dentro da classe, como um cachorro que seria um objeto dentro da classe Animal.
    
    A aula 10 foi a introdução da Utilização de variáveis, como elas funcionam e suas categorias, a professora explicou como funciona: Int, Float, char, String.
    
    A aula 11 Foi o aprofundamento da utilização das variáveis possíveis no Java, como: byte, Short, Long, Boolean, Pontos flutuantes, Utilização do ASCII, Pulo de linha com "\\", Limite da numeração do Java e etc.

---
+++ aula11 - Tipos Primitivos/VariáveisChar.java
@@ -0,0 +1,20 @@
+package com.loiane.cursojava.aula11;
+
+public class VariáveisChar {
+
+	public static void main(String[] args) {
+
+
+		// char o = 'o';
+		char o = 111;
+		
+		//char i = 'i';
+		char i = 105;
+		
+		char interrogacao = 0x00E1; // valor '?'
+		
+		System.out.println("" + o + i + interrogacao);
+
+	}
+
+}
